9895c1757944bacbd22348db69f0d77e935cb148
birukshegute <birukshegute@gmail.com>
2024-12-18 22:14:34 +0300

    Debugging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-console.log('hello world')
-let age:number = 30;
+console.log('hello world');
 
-async function hello() {
-    return ('world')
-}
+let age: number = 30;
+if (age < 50)
+    age+=10;
+console.log(age);
+
+// async function hello() {
+//     return ('world')
+// }
