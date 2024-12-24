b95aa49e2ece2411b97ac86890d6f1fc777f662f
birukshegute <birukshegute@gmail.com>
2024-12-25 00:03:36 +0300

    arrays

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,7 @@ let age = 30;
 if (age < 50)
     age += 10;
 console.log(age);
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toExponential);
+console.log(numbers);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ if (age < 50)
     age+=10;
 console.log(age);
 
+let numbers: number[] = [1, 2, 3]
+numbers.forEach(n=>n.toExponential)
+console.log(numbers)
+
+
 // async function hello() {
 //     return ('world')
 // }
