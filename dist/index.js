9e877d132992fa6b9fba4bf865236bfbf435719c
birukshegute <birukshegute@gmail.com>
2024-12-31 10:38:55 +0300

    enums

--- dist/index.js
+++ dist/index.js
@@ -7,5 +7,16 @@ console.log(age);
 let numbers = [1, 2, 3];
 numbers.forEach(n => n.toExponential);
 let user = [1, 'B'];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+;
+let mySize2 = 1;
+console.log(mySize, mySize2);
 console.log(numbers);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ console.log(age);
 let numbers: number[] = [1, 2, 3]
 numbers.forEach(n=>n.toExponential)
 let user: [number, string] = [1, 'B']
+enum Size { Small, Medium, Large};
+let mySize: Size = Size.Medium;
+const enum Size2 {Smaall, Medium, Large};
+let mySize2: Size2 = Size2.Medium
+console.log(mySize, mySize2);
 console.log(numbers)
 
 
