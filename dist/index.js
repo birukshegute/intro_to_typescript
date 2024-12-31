dcf2ac9b1f5557ef40535b22bc9339aca1dafed1
birukshegute <birukshegute@gmail.com>
2024-12-31 14:34:04 +0300

    union types

--- dist/index.js
+++ dist/index.js
@@ -33,4 +33,12 @@ let employee = {
     }
 };
 console.log(employee);
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,16 @@ let employee: Employee = {
 };
 
 console.log(employee)
+
+function kgToLbs(weight: number | string): number {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
 // async function hello() {
 //     return ('world')
 // }
